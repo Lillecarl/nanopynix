0878c20f06d23f5db591383119595c7f3fae9a1a
lillecarl <git@lillecarl.com>
2026-04-27 14:45:43 +0200

    Add format_exc_info processor to JSON log output
    Tracebacks from log.exception() and exc_info=True calls were
    being dropped by JSONRenderer. format_exc_info converts them to
    string representation before JSON serialization.

--- pynixd/__main__.py
+++ pynixd/__main__.py
@@ -65,6 +65,7 @@ def main() -> None:
             structlog.contextvars.merge_contextvars,
             structlog.processors.TimeStamper(fmt="iso"),
             structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
             structlog.processors.JSONRenderer(),
         ],
         logger_factory=structlog.stdlib.LoggerFactory(),
