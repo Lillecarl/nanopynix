a5d903aa2e1600f91f8d881f1f8c10d6e4977805
lillecarl <git@lillecarl.com>
2026-07-11 21:20:08 +0200

    feat: add nanopynix-proto subproject with gRPC proto definitions
    Define all nanopynix RPC types and services in .proto files, compiled
    via betterproto2 to Pydantic-compatible models. This is the foundation
    for migrating from custom JSON-RPC to gRPC over grpclib-transports.
    
    - proto/common.proto: shared messages (StorePath, PathInfo, BuildResult,
      enums, recursive wire types for DeepValue/CallArg, LogEvent, etc.)
    - proto/store.proto: StoreService with all 20 store RPC methods
    - proto/eval.proto: EvalService with all 21 eval/flake RPC methods
    - proto/worker.proto: WorkerService (Init, SubscribeLogs streaming, Shutdown)
    - proto/package.nix: Nix build running protoc + betterproto2-compiler
    - default.nix: wire betterproto2/compiler from grpclab, build nanopynix-proto,
      add nanopynix-proto + grpclib-transports as nanopynix dependencies

---
+++ proto/package.nix
@@ -0,0 +1,60 @@
+{
+  lib,
+  buildPythonPackage,
+  hatchling,
+  protobuf,
+  betterproto2,
+  betterproto2-compiler,
+  grpclib,
+  pydantic,
+}:
+
+buildPythonPackage {
+  pname = "nanopynix-proto";
+  version = "0.1.0";
+  pyproject = true;
+
+  src = lib.cleanSource ./.;
+
+  build-system = [ hatchling ];
+
+  nativeBuildInputs = [
+    protobuf
+    betterproto2-compiler
+    betterproto2
+    grpclib
+    pydantic
+  ];
+
+  dependencies = [
+    betterproto2
+    protobuf
+    pydantic
+    grpclib
+  ];
+
+  pythonImportsCheck = [
+    "nanopynix_proto"
+  ];
+
+  preBuild = ''
+    mkdir -p src/nanopynix_proto
+    touch src/nanopynix_proto/py.typed
+    protoc \
+      --proto_path=. \
+      --python_betterproto2_out=src \
+      --python_betterproto2_opt=client_generation=async \
+      --python_betterproto2_opt=server_generation=async \
+      --python_betterproto2_opt=google_protobuf_descriptors \
+      common.proto \
+      store.proto \
+      eval.proto \
+      worker.proto
+  '';
+
+  meta = with lib; {
+    description = "gRPC protobuf definitions for nanopynix (betterproto2 compiled)";
+    license = licenses.lgpl21Plus;
+    platforms = platforms.unix;
+  };
+}
