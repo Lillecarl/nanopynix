e31296afbefc1d8cbf918f485afa7c905cacea03
lillecarl <git@lillecarl.com>
2026-07-14 11:46:37 +0200

    Run deduplicated Nix version tests in CI

--- nix/nix-version-tests.nix
+++ nix/nix-version-tests.nix
@@ -3,12 +3,29 @@
   writeShellApplication,
   nanopynix-nixVersions,
 }:
+let
+  uniqueNanopynixVersions = lib.foldl' (
+    versions: nixVersion:
+    let
+      nanopynix = nanopynix-nixVersions.${nixVersion};
+      outputPathHash = builtins.hashString "sha256" (toString nanopynix);
+    in
+    if versions ? ${outputPathHash} then
+      versions
+    else
+      versions // {
+        ${outputPathHash} = {
+          inherit nixVersion nanopynix;
+        };
+      }
+  ) { } (lib.attrNames nanopynix-nixVersions);
+in
 writeShellApplication {
   name = "nanopynix-nix-version-tests";
   text = lib.concatStringsSep "\n" (
-    lib.mapAttrsToList (nixVersion: nanopynix: ''
+    lib.mapAttrsToList (_: { nixVersion, nanopynix }: ''
       echo "==> Testing nanopynix against ${nixVersion}"
       ${nanopynix.passthru.tests}/bin/nanopynix-tests "$@"
-    '') nanopynix-nixVersions
+    '') uniqueNanopynixVersions
   );
 }
