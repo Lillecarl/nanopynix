29b4fb5eb5d70cdffe9946be4445fb460162be51
lillecarl <git@lillecarl.com>
2026-07-19 16:44:48 +0200

    ci: split commit and scheduled workflows

---
+++ ci/workflows/on_commit.nix
@@ -0,0 +1,23 @@
+let
+  workflow = import ./lib.nix { };
+  inherit (workflow.ciLib) mkWorkflow withCond;
+
+  testJobs = workflow.mkStaticTestJobs { };
+  tsanTestJobs = workflow.mkStaticTsanTestJobs { };
+  allTestJobs = testJobs // tsanTestJobs;
+in
+mkWorkflow {
+  name = "On commit";
+  on = {
+    push = null;
+    workflow_dispatch = null;
+  };
+  jobs = allTestJobs // {
+    docs-build = withCond "github.ref == 'refs/heads/develop'" (workflow.mkDocsBuildJob {
+      needs = builtins.attrNames allTestJobs;
+    });
+    docs-deploy = withCond "github.ref == 'refs/heads/develop'" (workflow.mkDocsDeployJob {
+      needs = "docs-build";
+    });
+  };
+}
